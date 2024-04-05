0d84db6b5cf44e19ec558ee029199e25be87e260
Park, Sojeong <sojeong2101@gmail.com>
2024-04-05 23:54:37 +0900

    :sparkles: [feat] 출력 내용 추가
    - 계산된 거리에 따라 [자동차 이름 : '-' * n] 형식으로 출력 기능 구현했습니다.
    - 우승자 출력 기능을 구현했습니다.

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,48 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final int SOLO_WINNER = 1;
+
+    public void printAllCarMovement(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(printEachCarMovement(car));
+        }
+    }
+
+    public void printWinners(List<Car> winnerCars) {
+        StringBuilder nameOfWinnerCars = new StringBuilder();
+
+        if (isSoleWinner(winnerCars)) {
+            System.out.println("최종 우승자: " + nameOfWinnerCars);
+            return;
+        }
+
+        for (Car car : winnerCars) {
+            nameOfWinnerCars.append(car.getName()).append(", ");
+        }
+
+        nameOfWinnerCars.deleteCharAt(nameOfWinnerCars.length() - 1);
+        System.out.println("최종 우승자: " + nameOfWinnerCars);
+    }
+
+    private String printEachCarMovement(Car car) {
+        return car.getName() + printPosition(car.getTotalMovement);
+    }
+
+    private String printPosition(int movementNumber) {
+        String totalDash = "";
+
+        for (int i = 0; i <= movementNumber; i++) {
+            totalDash += "-";
+        }
+        return totalDash;
+    }
+
+    private boolean isSoleWinner(List<Car> winnerCars) {
+        return winnerCars.size() == SOLO_WINNER;
+    }
+
+}
